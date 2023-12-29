7175f1b9350cae28726a0ba75c842d19d58863a5
Cesar Leal Hernández <cesar.leal@softtekprojects.com>
2023-12-29 11:17:22 -0600

    Integracion de la vista principal de la lista de eventos y presentacion

--- lit-components/pantalla-principal-styles.js
+++ lit-components/pantalla-principal-styles.js
@@ -1,8 +1,48 @@
 import { css } from "lit";
 
 export default css `
-    p {
-        font-size:32px;
-        color: orange;
-    }
+        :host{
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            text-align: center;
+        }
+
+        button{
+            background-color: rgb(216, 216, 216);
+            padding: 0.4rem 0.8rem;
+            border: none;
+            border-radius: 4px;
+            box-shadow: 2px 2px 3px rgba(128, 128, 128, 0.678);
+            cursor: pointer;
+            display:block;
+            margin:auto;
+        }
+
+        button:active{
+            border: solid 1px rgb(186, 186, 186);
+            box-shadow: 3px 3px 3px rgba(128, 128, 128, 0.922);
+        }
+
+        nav > h1{
+            color: white;
+        }
+
+        .eventos{
+            display: flex;
+            justify-content: center;
+            margin-top: 2rem;
+        }
+
+        .item-evento{
+            background-color: rgba(219, 219, 219, 0.35);
+            width: 60%;
+            border-radius: 10px;
+            display: flex;
+            justify-content: space-between;
+            padding: 0.2rem 1.5rem;   
+
+        }
+    
+        .eliminar-eventos{
+            cursor:pointer;
+        }
 `
--- lit-components/pantalla-principal.js
+++ lit-components/pantalla-principal.js
@@ -1,25 +1,41 @@
 import { LitElement, html, css } from 'lit';
-import  styles  from "./pantalla-principal-styles";
+import styles from "./pantalla-principal-styles";
 
 export class PantallaPrincipal extends LitElement {
-   
-    static styles=[
+
+    static styles = [
         styles
     ]
-    
+
     static get properties() {
         return {
             name: { type: String },
         };
     }
 
-    constructor(){
+    constructor() {
         super();
         this.name = 'Luis'
     }
 
     render() {
-        return html``;
+        return html`
+        <nav>
+            <h1>Gestor de eventos</h1>
+            <p>
+                Proyecto realizado con el proposito de poner a prueba los conocimientos adquiridos en el curso
+                de Lit element por parte de Softek.
+            </p>
+        </nav>
+        <button type="button"><strong>+</strong> Agendar nuevo evento</button>
+        <section class="eventos">
+            <div class="item-evento">
+                <p>Un evento de un día festivo</p>
+                <p>24 de febrero de 2023 a las 12:30 hrs.</p>
+            </div>
+        </section>
+        <h4 class="eliminar-eventos">Eliminar todos los eventos</h4>
+        `;
     }
 }
 customElements.define('pantalla-principal', PantallaPrincipal);
